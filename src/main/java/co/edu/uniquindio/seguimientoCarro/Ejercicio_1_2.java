0063ca13f00cda84fe82930568debe19e5b07218
24Valentina <karenv.valbuenar@uqvirtual.edu.co>
2024-10-03 16:07:05 -0500

    Se crea baseDatos.properties, con el main llamado ejercicio_3

---
+++ src/main/java/co/edu/uniquindio/seguimientoCarro/Ejercicio_1_2.java
@@ -0,0 +1,25 @@
+package co.edu.uniquindio.seguimientoCarro;
+
+import co.edu.uniquindio.seguimientoCarro.utils.ArchivoUtils;
+
+import java.io.IOException;
+
+public class ejercicio_1_2 {
+    public static void main(String[] args) throws IOException {
+
+        String archivoRutaAbsoluta = "C:\\Users\\ASUS\\OneDrive\\Documentos\\Programacion_III\\co.edu.uniquindio.seguimientoCarro\\src\\main\\resources\\Persistencia\\archivoCarros.txt";
+        String archivoRutaRelativa = "archivoCarros.txt";
+
+        ArchivoUtils.leerArchivo("C:\\Users\\ASUS\\OneDrive\\Documentos\\Programacion_III\\co.edu.uniquindio.seguimientoCarro\\src\\main\\resources\\Persistencia\\archivoCarros.txt");
+
+        //ArchivoUtils.escribirArchivo("C:\\Users\\ASUS\\OneDrive\\Documentos\\Programacion_III\\co.edu.uniquindio.seguimientoCarro\\src\\main\\resources\\Persistencia\\archivoCarros.txt",
+                //"Lamborghini;Aventador SVJ;Verde; 6.5L V12, 770 hp");
+
+        //ArchivoUtils.agregarCarro("C:\\Users\\ASUS\\OneDrive\\Documentos\\Programacion_III\\co.edu.uniquindio.seguimientoCarro\\src\\main\\resources\\Persistencia\\archivoCarros.txt",
+               //"Hyundai;Santa Fe;Gris metalizado;2.5L Turbo");
+
+        //ArchivoUtils.guardarArchivo(archivoRutaAbsoluta, "otro carro", true);
+        //ArchivoUtils.guardarArchivo(archivoRutaRelativa, "otro carro", true);
+
+    }
+}
--- src/main/java/co/edu/uniquindio/seguimientoCarro/ejercicio_3.java
+++
@@ -1,9 +0,0 @@
-package co.edu.uniquindio.seguimientoCarro;
-
-import java.io.IOException;
-
-public class ejercicio_3 {
-    public static void main(String[] args) throws IOException {
-// esta vacio 2
-    }
-}
